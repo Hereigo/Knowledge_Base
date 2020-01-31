fd7d70ad73e32c7f908bcdfd47ce47a9f9d60808
Andrii Plakhtii <andrii.plakhtii@eleks.com>
2020-01-31 14:11:34 +0200

    js-aliex 2 update 3

--- JS_AliExpress/script/script.js
+++ JS_AliExpress/script/script.js
@@ -6,17 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const search = document.querySelector('.search');
     const searchBtn = document.getElementById('wishlist');
 
-    fetch('db/db.json').then((rez) => {
-        
-        console.log(rez.json());
-
-    });
-
-
-
-    // ------------ 1:05:00 
-
-
     // this function does NOT available until line 13 !
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
@@ -37,6 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>`;
         return card;
     };
+
+
 
     goodsWrapper.appendChild(createCardGoods(111, 'Adfkj hd', 100000, 'Archer.jpg'));
     goodsWrapper.appendChild(createCardGoods(222, 'Dkldhb DIbh', 200000, 'Flamingo.jpg'));
@@ -65,10 +56,23 @@ document.addEventListener('DOMContentLoaded', function () {
     cardBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
 
+    const getGoods = (handler) => {
+        fetch('db/db.json')
+            .then((rez) => { return rez.json(); })
+            // .then((goods) => { console.log(goods[3]); })
+            .then(handler);
+    }
+
+    const renderCard = cardsArray => {
+        cardsArray.forEach((elem, ind, fullArr) => {
+            console.log(++ind, elem.title.substring(0,15).tou +'...', elem);
+        });
+    };
+
+    getGoods(renderCard);
 
 
-
-
+    // ------------ 1:18:00 
 
 
     //
