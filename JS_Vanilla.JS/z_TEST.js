29a356d464b792c9393687e509c2f4e487925b7b
Andrii Plakhtii <andrii.plakhtii@eleks.com>
2020-02-05 10:35:31 +0200

    small save

--- JS_Vanilla.JS/z_TEST.js
+++ JS_Vanilla.JS/z_TEST.js
@@ -11,7 +11,7 @@ console.log(reservedWords.for + reservedWords.let + reservedWords.return);
 let uno = 100;
 
 for (let i = 0; i < 9; i++) {
-    console.log(i + ' - ' + (uno >> i) + ' - ' + (uno << i));
+  console.log(i + ' - ' + (uno >> i) + ' - ' + (uno << i));
 }
 // x >> i == (x/2) i times, x << i == (x*2) i times
 
@@ -21,8 +21,13 @@ let x = 5;
 
 x = (x++ , x = addFive(x), x *= 2, x -= 5, x += 10);
 
-function addFive(num){
-    return num + 5;
+function addFive(num) {
+  return num + 5;
 }
 
 console.log(x);
+
+// ===========================
+
+console.log([].propertyIsEnumerable(0));     // FALSE
+console.log(['bbb'].propertyIsEnumerable(0)); // TRUE      
