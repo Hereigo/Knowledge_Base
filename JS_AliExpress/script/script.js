da328fe28acbcd42a0a55d9e9188a99f63146d68
Andrii Plakhtii <andrii.plakhtii@eleks.com>
2020-02-10 12:00:28 +0200

    sj-aliex small update

--- JS_AliExpress/script/script.js
+++ JS_AliExpress/script/script.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const goodsWrapper = document.querySelector('.goods-wrapper');
     const search = document.querySelector('.search');
     const searchBtn = document.getElementById('wishlist');
+    const category = document.querySelector('.category');
 
-    // this function does NOT available until line 13 !
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
         card.innerHTML =
             `<div class="card">
                 <div class="card-img-wrapper">
-                    <img class="card-img-top" src="img/temp/${img}" alt="">
+                    <img class="card-img-top" src="${img}" alt="">
                     <button class="card-add-wishlist" data-goods-id="${id}"></button>
                 </div>
                 <div class="card-body justify-content-between">
@@ -27,25 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
         return card;
     };
 
-
-
-    goodsWrapper.appendChild(createCardGoods(111, 'Adfkj hd', 100000, 'Archer.jpg'));
-    goodsWrapper.appendChild(createCardGoods(222, 'Dkldhb DIbh', 200000, 'Flamingo.jpg'));
-    goodsWrapper.appendChild(createCardGoods(333, 'Dljbhuyobyuo', 300000, 'Socks.jpg'));
-
-    const openCart = (event) => {
-
+    const openCart = event => {
         // force prevent reference click action :
         event.preventDefault();
-
         cart.style.display = 'flex';
         document.addEventListener('keyup', closeCart);
     }
 
-    const closeCart = (event) => {
-
+    const closeCart = event => {
         const target = event.target;
-
         // on Esc key , on Cart click , on cart-close Cross sign :
         if (event.keyCode === 27 || target === cart || target.classList.contains('cart-close')) {
             cart.style.display = '';
@@ -53,26 +43,43 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    const chooseCategory = event => {
+        event.preventDefault();
+        const target = event.target;
+        if(target.classList.contains('category-item')){
+            console.log(target.dataset);
+        }
+    };
+
     cardBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
+    category.addEventListener('click', chooseCategory);
 
-    const getGoods = (handler) => {
+
+
+    const getGoods = (handler, filter) => {
         fetch('db/db.json')
             .then((rez) => { return rez.json(); })
             // .then((goods) => { console.log(goods[3]); })
+            .then(filter)
             .then(handler);
     }
 
     const renderCard = cardsArray => {
-        cardsArray.forEach((elem, ind, fullArr) => {
-            console.log(++ind, elem.title.substring(0,15).tou +'...', elem);
+        cardsArray.forEach((arrayElem) => {
+            const { id, title, price, imgMin } = arrayElem;
+            goodsWrapper.appendChild(
+                createCardGoods(id, title, price, imgMin)
+            );
         });
     };
 
-    getGoods(renderCard);
+    const randomSort = (array) => array.sort(() => Math.random() - 0.5);
+
+    getGoods(renderCard, randomSort);
 
 
-    // ------------ 1:18:00 
+    // ------------ 2:11:00 
 
 
     //
