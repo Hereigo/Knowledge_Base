f1f6dfa8ded69c03cf85d407ee07cc54ed533cf4
Andrii Plakhtii <andrii.plakhtii@eleks.com>
2020-02-11 16:24:06 +0200

    angula-js mvc4 add model

--- AngularJS_AspNet_MVC4/App_Start/Startup.Auth.cs
+++ AngularJS_AspNet_MVC4/App_Start/Startup.Auth.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using AngularJS_AspNet_MVC4.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using AngularJS_AspNet_MVC4.Models;
 
 namespace AngularJS_AspNet_MVC4
 {
@@ -34,7 +33,7 @@ namespace AngularJS_AspNet_MVC4
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
