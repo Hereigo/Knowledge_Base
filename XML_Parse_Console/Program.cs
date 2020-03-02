03b6c35a28265e8c183f09793d39a2625aff2a40
Andrii Plakhtii <andrii.plakhtii@eleks.com>
2020-03-02 10:13:00 +0200

    xml parse begin

---
+++ XML_Parse_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_Parse_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
