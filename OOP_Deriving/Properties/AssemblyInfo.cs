c4926fa4fe6cafb329bb791b16ff5bb18e41bece
andrii.plakhtii <andrii.plakhtii@eleks.com>
2020-04-30 08:13:04 +0300

    add csharp linq and oop

---
+++ OOP_Deriving/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OOP_Deriving")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP_Deriving")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28cfb1e8-520b-4cab-b6b0-51c5353a9be2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OOP_Deriving/Use_Base.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP_Deriving
+{
+    public static class Use_Base
+    {
+        public abstract class ParentAbstract
+        {
+            public virtual void Method()
+            {
+                Console.WriteLine("Virtual Method In Abstract Parent.");
+            }
+        }
+
+        public class ChildClass : ParentAbstract
+        {
+            public override void Method()
+            {
+                Console.WriteLine("Call - base.Method() : ");
+
+                base.Method();
+
+                // this.Method(); - will call infinite recursion
+
+                Console.WriteLine();
+
+                Console.WriteLine("Overriden Method In Child Class.");
+            }
+        }
+
+        public static void Test()
+        {
+            ParentAbstract item = new ChildClass();
+
+            item.Method();
+        }
+    }
+}
