8f421607bad589cd3df46c328797d4b4e44984c7
andrii.plakhtii <andrii.plakhtii@eleks.com>
2020-09-08 15:02:04 +0300

    refactoring

--- AngularJS_CA/directives/itemMark.js
+++ AngularJS_CA/directives/itemMark.js
@@ -4,22 +4,19 @@ app.directive('itemMark', function () {
         restrict: 'E',
         scope: {},
         templateUrl: 'directives/itemMark.html',
-
         link: function (scope, element, attrs) {
-
             scope.buttonText = "Mark",
-                scope.installed = false,
-                scope.markItem = function () {
-                    element.toggleClass('btn-active');
-                    if (scope.installed) {
-                        scope.buttonText = "Mark";
-                        scope.installed = false;
-                    } else {
-                        scope.buttonText = "Unmark";
-                        scope.installed = true;
-                    }
+            scope.installed = false,
+            scope.markItem = function () {
+                element.toggleClass('btn-active');
+                if (scope.installed) {
+                    scope.buttonText = "Mark";
+                    scope.installed = false;
+                } else {
+                    scope.buttonText = "Unmark";
+                    scope.installed = true;
                 }
+            }
         }
     };
-
 });
