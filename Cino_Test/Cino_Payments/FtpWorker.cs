28e3b2a776931727fcd342eb890157809b26d676
hereigo <hereigo@online.ua>
2020-11-06 16:13:10 +0200

    cino test

---
+++ Cino_Test/Cino_Payments/FtpWorker.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace Cino_Payments
+{
+    public class FtpWorker
+    {
+        private readonly NetworkCredential credentials;
+
+        public FtpWorker(string password)
+        {
+            credentials = new NetworkCredential(GIT_IGNORE.ftpUser, password);
+        }
+
+        internal string[] GetFilesListByExt(Uri remoteUri, string fileExtension)
+        {
+            List<string> filesList = new List<string>();
+
+            // The serverUri parameter should start with the ftp:// scheme.
+            if (remoteUri.Scheme != Uri.UriSchemeFtp)
+            {
+                Console.WriteLine($"Not valid FTP url : {remoteUri}");
+            }
+            else
+            {
+                try
+                {
+                    // Get the object used to communicate with the server.
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create($"{remoteUri}");
+                    request.Credentials = credentials;
+                    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+                    FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                    Stream responseStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(responseStream);
+                    string names = reader.ReadToEnd();
+
+                    reader.Close();
+                    response.Close();
+
+                    // select zip-file name from the latest file record :
+                    foreach (var line in names.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToArray())
+                    {
+                        foreach (var item in line.Split(' '))
+                        {
+                            if (item.EndsWith(fileExtension, StringComparison.OrdinalIgnoreCase))
+                            {
+                                filesList.Add(item);
+                            }
+                        }
+                    }
+
+                    Console.WriteLine($"\r\nDirectory List Complete, status {response.StatusDescription}");
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            return filesList.ToArray();
+        }
+
+        internal void DeleteFilesFromFtp(string[] filesList, Uri remoteUri)
+        {
+            foreach (var file in filesList)
+            {
+                try
+                {
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create($"{remoteUri}{file}");
+                    request.Credentials = credentials;
+                    request.Method = WebRequestMethods.Ftp.DeleteFile;
+                    FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                    Console.WriteLine($"{file} - Delete status: {response.StatusDescription}");
+                    response.Close();
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        internal void DownloadFileFromFtp(string fileName, Uri remoteUri, bool shouldToOpen)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Console.WriteLine("No files to download!");
+                return;
+            }
+
+            WebClient myWebClient = new WebClient
+            {
+                Credentials = credentials
+            };
+
+            try
+            {
+                Console.WriteLine($"Downloading File \"{remoteUri}\" .......\n\n");
+
+                myWebClient.DownloadFile(remoteUri + fileName, fileName);
+
+                Console.WriteLine($"File {fileName} Successfully Downloaded.");
+
+                if (shouldToOpen)
+                {
+                    Process.Start(fileName);
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        internal string SelectLatestByNameFile(string[] filesList)
+        {
+            Array.Reverse(filesList);
+
+            return filesList.Length > 0 ? filesList[0] : string.Empty;
+        }
+    }
+}
---
+++ Cino_Test/Cino_Payments/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace Cino_Payments
+{
+    class Program
+    {
+        private static Uri uriArch = new Uri(GIT_IGNORE.ftpPath);
+        private static Uri uriDb = new Uri(GIT_IGNORE.ftpPathDb);
+
+        private static void Main()
+        {
+            // Archiving previous :
+
+//            if (File.Exists(GIT_IGNORE.zipOutputDB))
+//            {
+//                string oldFile = GIT_IGNORE.tempPathToStore + GIT_IGNORE.zipOutputDB;
+//                if (File.Exists(oldFile))
+//                {
+//                    File.Delete(oldFile);
+//                }
+//                File.Move(GIT_IGNORE.zipOutputDB, oldFile);
+//            }
+//
+//            if (File.Exists(GIT_IGNORE.zipOutput))
+//            {
+//                string oldFile = GIT_IGNORE.tempPathToStore + GIT_IGNORE.zipOutput;
+//                if (File.Exists(oldFile))
+//                {
+//                    File.Delete(oldFile);
+//                }
+//                File.Move(GIT_IGNORE.zipOutput, oldFile);
+//            }
+
+            Console.WriteLine("\r\n - What would you like, my Lord? \r\n");
+
+            string password = Console.ReadLine().Trim();
+
+            FtpWorker ftp = new FtpWorker(password);
+            Zipper zipper = new Zipper(password);
+
+            string[] ftpFilesBak = ftp.GetFilesListByExt(uriDb, ".bak");
+
+            string ftpFilesBakLast = string.Empty;
+
+            foreach (string item in ftpFilesBak)
+            {
+                if (!File.Exists(item))
+                {
+                    ftp.DownloadFileFromFtp(item, uriDb, false);
+                    ftpFilesBakLast = item;
+                }
+            }
+
+            zipper.CompressFile(ftpFilesBakLast, GIT_IGNORE.zipOutputDB);
+
+            string[] archivesList = ftp.GetFilesListByExt(uriArch, ".zip");
+
+            if (archivesList.Length > 0)
+            {
+                string latestArchiveName = ftp.SelectLatestByNameFile(archivesList);
+
+                ftp.DownloadFileFromFtp(latestArchiveName, uriArch, true);
+
+                zipper.CompressFile(latestArchiveName, GIT_IGNORE.zipOutput);
+
+                FileInfo archive = new FileInfo(GIT_IGNORE.zipOutput);
+
+                if (archive.Exists)
+                {
+                    Console.WriteLine($"\r\nArchive Created. Size : {archive.Length / 1024} kb, Modified : {archive.LastWriteTime}");
+                    Console.WriteLine("\r\nCheck downloaded file and press any key TO DELETE temporary files ...\r\n");
+                    Console.ReadKey();
+                    File.Delete(latestArchiveName);
+                    ftp.DeleteFilesFromFtp(archivesList, uriArch);
+                }
+                else
+                {
+                    Console.WriteLine("WARNING! Archive not created!");
+                }
+            }
+
+            Console.WriteLine("\r\nAll tasks finished.");
+            Console.ReadKey();
+        }
+    }
+}
