1f67da69897908c609b1420f57661994503857a5
hereigo <hereigo@online.ua>
2020-11-27 19:18:19 +0200

    test smart-git updates

--- CinoSomMVC/Startup.cs
+++ CinoSomMVC/Startup.cs
@@ -20,13 +20,13 @@ namespace CinoSomMVC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Add Services (called by the runtime).
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline (called by runtime).
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,11 +36,13 @@ namespace CinoSomMVC
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
+                // The default HSTS value is 30 days (https://aka.ms/aspnetcore-hsts).
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
