a998100dc6560ea7a8ff2c341df0f2cb09ac222d
hereigo <hereigo@online.ua>
2021-01-09 16:18:02 +0200

    add cino-pays-auth

---
+++ CinoPaysAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CinoPaysAuth.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CinoPaysAuth.Areas.Identity.IdentityHostingStartup))]
+namespace CinoPaysAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ CinoPaysAuth/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using CinoPaysAuth.Models;
+using System.Collections.Generic;
+
+namespace CinoPaysAuth.Services
+{
+    public class EmployeeService
+    {
+        Employees emps;
+
+        public EmployeeService(Employees emps)
+        {
+            this.emps = emps;
+        }
+        public List<Employee> Get()
+        {
+            return emps;
+        }
+
+        public List<Employee> Create(Employee emp)
+        {
+            emps.Add(emp);
+            return emps;
+        }
+    }
+}
