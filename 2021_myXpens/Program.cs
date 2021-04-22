9b6f0d5c8b03ad4f80d8a1a8487521d4694d2bb5
Hereigo <Hereigo@online.ua>
2021-04-22 13:51:24 +0300

    clear useless projects

--- 2021_myXpens/Program.cs
+++ 2021_myXpens/Program.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MyXpens
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -18,6 +12,12 @@ namespace MyXpens
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                                .ConfigureWebHostDefaults(webBuilder =>
+                                {
+                                    webBuilder.UseStartup<Startup>();
+                                    // webBuilder.UseUrls("http://192.168.0.105:5003");
+                                    // To call from outside don't forget to switch firewall :
+                                    // sudo ufw [allow\deny] in 5003 
+                                });
     }
 }
