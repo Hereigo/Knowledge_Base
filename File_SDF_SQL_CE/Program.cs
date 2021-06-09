df62401b57a8a7b2431baac7a2188228d3357334
Hereigo <Hereigo@online.ua>
2021-06-09 18:52:44 +0300

    small update

---
+++ File_SDF_SQL_CE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_SDF_SQL_CE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
