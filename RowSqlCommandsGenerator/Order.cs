c3c46bb7c0a2a0d56df7d689bd2965b7f5478440
Hereigo <Hereigo@online.ua>
2021-07-14 11:14:53 +0300

    update raw sql gen + packages

--- RowSqlCommandsGenerator/Order.cs
+++ RowSqlCommandsGenerator/Order.cs
@@ -1,10 +1,17 @@
-﻿namespace RawSqlCommandsGenerator
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RawSqlCommandsGenerator
 {
     public class Order
     {
+        [Column("ID")]
         public int OrderId { get; set; }
+
         public int ProductId { get; set; }
+
+        [Column("Description")]
         public string ProductName { get; set; }
+
         public decimal ProductPrice { get; set; }
     }
 }
--- RowSqlCommandsGenerator/RawSqlGenerator.cs
+++ RowSqlCommandsGenerator/RawSqlGenerator.cs
@@ -1,15 +1,38 @@
-﻿namespace RawSqlCommandsGenerator
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RawSqlCommandsGenerator
 {
     public class RawSqlGenerator
     {
         public string InsertIntoTableNameByEntity<T>(T entity)
         {
-            string resultFirst = $" INSERT INTO [{entity.GetType().Name}] (";
-            string resultSecond = ") VALUES (";
+            var resultFirst = $" INSERT INTO [{entity.GetType().Name}] (";
+            var resultSecond = ") VALUES (";
 
-            foreach (var property in entity.GetType().GetProperties())
+            var _attrDict = new Dictionary<string, string>();
+
+            var properties = entity.GetType().GetProperties();
+
+            foreach (var property in properties)
             {
-                resultFirst += $" {property.Name},";
+                foreach (var attr in property.GetCustomAttributes(true))
+                {
+                    if (attr is ColumnAttribute authAttr)
+                    {
+                        var propName = property.Name;
+                        var auth = authAttr.Name;
+
+                        _attrDict.Add(propName, auth);
+                    }
+                }
+            }
+
+            foreach (var property in properties)
+            {
+                resultFirst += _attrDict.ContainsKey(property.Name)
+                    ? $" {_attrDict[property.Name]},"
+                    : $" {property.Name},";
 
                 resultSecond += property.PropertyType.Equals(typeof(string))
                     ? $" '{property.GetValue(entity, null)}',"
