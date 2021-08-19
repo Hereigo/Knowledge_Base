a45a42835d04a53469ff5636604db4a3595a5f72
Hereigo <Hereigo@online.ua>
2021-08-19 22:00:40 +0300

    add quartz.net

---
+++ QuartzNet_Use/ConsoleLogProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Quartz.Logging;
+
+namespace QuartzNet_Use
+{
+    public class ConsoleLogProvider : ILogProvider
+    {
+        public Logger GetLogger(string name)
+        {
+            return (level, func, exception, parameters) =>
+            {
+                if (level >= LogLevel.Info && func != null)
+                {
+                    Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
+                }
+                return true;
+            };
+        }
+
+        public IDisposable OpenNestedContext(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable OpenMappedContext(string key, object value, bool destructure = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
