0fa0b3248645b6bde8958b8f77fee8fefa224cee
Hereigo <Hereigo@users.noreply.github.com>
2021-10-08 20:02:12 +0300

    ng data-binding

--- Angular11/xpns-app/src/app/app.component.ts
+++ Angular11/xpns-app/src/app/app.component.ts
@@ -7,10 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  aaaProperty = 'Component-Property';
-  
-  constructor() { }
-  
+  constructor() {
+    this.startInterval();
+  }
+
   ngOnInit(): void { }
-  
+
+  aaaProperty = 'Component-Property';
+  counter: number = 0;
+  isChecked = true;
+  isCounterOn = true;
+  myTextSpanValue = ' - Type and press Enter.';
+
+  startInterval() {
+    setInterval(() => {
+      if (this.isCounterOn) {
+        this.counter++;
+        if (this.counter % 2 == 0) this.isChecked = true;
+        else this.isChecked = false;
+      }
+    }, 1000);
+  }
+
+  switchCounter() {
+    this.isCounterOn = !this.isCounterOn;
+  }
+
+  grabTextInput(text: { toString: () => any; }) {
+    this.myTextSpanValue = ' - ' + text.toString();
+  }
+
 }
