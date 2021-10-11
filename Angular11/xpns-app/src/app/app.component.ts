6a902ec95edba0e12ab76314d2e749d0353f0590
Hereigo <Hereigo@users.noreply.github.com>
2021-10-11 12:43:18 +0300

    ng binding 2

--- Angular11/xpns-app/src/app/app.component.ts
+++ Angular11/xpns-app/src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   isChecked = true;
   isCounterOn = true;
   myTextSpanValue = ' - Type and press Enter.';
+  toggle = true;
 
   startInterval() {
     setInterval(() => {
