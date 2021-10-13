81c68ce9bdc9f37e5c172ec1444bc4beeaf8f2b6
Hereigo <Hereigo@users.noreply.github.com>
2021-10-13 15:12:11 +0300

    ng styles inheritance

--- Angular11/xpns-app/src/app/02-styles-inheritance/styles-inheritance.module.ts
+++ Angular11/xpns-app/src/app/02-styles-inheritance/styles-inheritance.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StylesInheritanceComponent } from './styles-inheritance.component';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    StylesInheritanceComponent,
-    ChildComponent
+    StylesInheritanceComponent
   ],
   imports: [
     CommonModule
