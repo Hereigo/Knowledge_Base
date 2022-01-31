a962d9f8818edaca62962f8df9c1dd8dc8184749
Hereigo <hereigo@online.ua>
2022-01-31 15:19:39 +0200

    reorganize solution

--- AngularJS_AspNet_Bootstrap/Models/EmployeeModel.Designer.cs
+++ AngularJS_AspNet_Bootstrap/Models/EmployeeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\z510idea\source\repos\Knowledge_Base\AngularJS_AspNet_Bootstrap\Models\EmployeeModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\aaa\Source\Repos\Hereigo\Knowledge_Base\AngularJS_AspNet_Bootstrap\Models\EmployeeModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
